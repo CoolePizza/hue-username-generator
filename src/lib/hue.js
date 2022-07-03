0a3fd47b05dd6b99c5af80aa15fb11d9c12638d6
CoolePizza <hello@coolepizza.de>
2022-07-03 13:51:41 +0200

    try cors

--- src/lib/hue.js
+++ src/lib/hue.js
@@ -1,6 +1,7 @@
 export async function createUser(ip, devicetype) {
     const response = await fetch(`https://${ip}/api`, {
         method: "POST",
+        mode: "cors",
         body: JSON.stringify({
             devicetype
         })
